66b9fad538249af65502ad1298a34c6d40d34192
Vince <vincent_liu_5@sfu.ca>
2025-09-21 17:34:54 -0700

    lesson progress sep 13

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const catchBtn = document.getElementById("catch-btn");
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonImg = document.getElementById("pokemon-img");
+const shinyImg = document.getElementById("shiny-img");
 const pokemonTypes = document.getElementById("pokemon-types");
 
 function getRandomPokemonId() {
-  // TODO: generate random number between 1 and 1025
-  return 1; // placeholder
+  return Math.floor(Math.random() * 151) + 1;
 }
 
 async function fetchPokemon(id) {
   // TODO: fetch data from PokéAPI using the id
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const pokemonData = await response.json();
+  console.log(pokemonData);
+
+  return {
+    name: pokemonData.name,
+    sprite: pokemonData.sprites.front_default,
+    shiny_sprite: pokemonData.sprites.front_shiny,
+    types: pokemonData.types.map((typeInfo) => typeInfo.type.name),
+  };
 }
 
 async function displayPokemon() {
   const id = getRandomPokemonId();
   const pokemon = await fetchPokemon(id);
 
-  // TODO: update DOM with real data
+  // Update DOM
   pokemonName.textContent = pokemon.name;
   pokemonImg.src = pokemon.sprite || "";
+  shinyImg.src = pokemon.shiny_sprite || "";
   pokemonTypes.textContent = "Type: " + pokemon.types.join(", ");
 }
 
